fc0c5cf34bc922ff831eac7afc5be4ce672c02a7
Murat Tekin <Zxmod51@gmx.de>
2023-09-23 15:07:45 +0200

    Adjust classnames because overlapping css behaviour

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode className="App">
+  <React.StrictMode className="Application">
     <BrowserRouter>
     <App />
     </BrowserRouter>
